61bc984ed6f7674ec5f0c34dec2da758abdca316
Oshith Roshantha <oedirisuriya@gmail.com>
2025-03-15 19:33:57 +0530

    Refactor clvPreProcess function to correctly aggregate customer data and improve readability

--- app/models/clvModel.py
+++ app/models/clvModel.py
@@ -7,7 +7,13 @@ from sklearn.preprocessing import MinMaxScaler
 import joblib
 
 def clvPreProcess(data):
-    dfGrouped = data.groupBy('Customer_ID').agg('Customer_Segment').alias('Type'), F.round(F.sum("Total_Amount") ,2).alias("Total_Spend"), F.sum("Total_Purchases").alias("Total_Purchases"), F.min("Date").alias("First_Purchase_Date"), F.max("Date").alias("Last_Purchase_Date"))
+    dfGrouped = data.groupBy('Customer_ID').agg(
+        F.first('Customer_Segment').alias('Type'), 
+        F.round(F.sum("Total_Amount"), 2).alias("Total_Spend"),
+        F.sum("Total_Purchases").alias("Total_Purchases"),
+        F.min("Date").alias("First_Purchase_Date"),
+        F.max("Date").alias("Last_Purchase_Date")
+    )
     lifeSpanDf = dfGrouped.withColumn('Lifespan', F.round(((F.datediff(F.col("Last_Purchase_Date"), F.col("First_Purchase_Date")))/365), 2))
     lifeSpanDf = lifeSpanDf.drop('First_Purchase_Date', 'Last_Purchase_Date', 'Customer_ID')
     clvDf = lifeSpanDf.withColumn('CLV', F.round(((F.col('Total_Purchases'))/(F.col('Lifespan')))*(F.col('Total_Spend')), 2))
