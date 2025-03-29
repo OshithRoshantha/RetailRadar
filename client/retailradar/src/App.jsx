4dc5652fc17f2e0df31be59a75ed9a96ab2d234a
Oshith Roshantha <oedirisuriya@gmail.com>
2025-03-29 11:14:21 +0530

    Remove unused files and components from node_modules and add SignIn component

--- client/retailradar/src/App.jsx
+++ client/retailradar/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import SignIn from './pages/SignIn';
 
 function App() {
 
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path='/' element={<SignIn/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
