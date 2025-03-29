c84586a5c24a41e3a7be57a54dd5b841623228fc
Oshith Roshantha <oedirisuriya@gmail.com>
2025-03-29 12:20:12 +0530

    Migrate project to TypeScript; add SignIn component and update routing; remove unused files and assets

---
+++ client/retailradar/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import SignIn from './pages/SignIn';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<SignIn/>} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
