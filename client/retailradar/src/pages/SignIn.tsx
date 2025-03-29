541871090cfd53522c144eff84f8f5c59a3e6726
Oshith Roshantha <oedirisuriya@gmail.com>
2025-03-29 12:34:16 +0530

    Add logo to SignIn page and create corresponding CSS for styling

--- client/retailradar/src/pages/SignIn.tsx
+++ client/retailradar/src/pages/SignIn.tsx
@@ -1,17 +1,22 @@
 import { LoginForm } from "@/components/LoginForm";
 import { GalleryVerticalEnd } from "lucide-react"
+import mainLogo from '../assets/logo/logo1.png'
+import './css/SignIn.css'
 
 export default function SignIn() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <div className="flex items-center gap-2 font-medium">
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
             </div>
-            Acme Inc.
-          </a>
+                <img
+                src={mainLogo}
+                alt="Image"
+                className="main-logo"
+                />
+          </div>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
@@ -20,11 +25,7 @@ export default function SignIn() {
         </div>
       </div>
       <div className="relative hidden bg-muted lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+
       </div>
     </div>
   )
