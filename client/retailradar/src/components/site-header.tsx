f6db056aceb42b2d25d22f1e02bcca46924bb166
Oshith Roshantha <oedirisuriya@gmail.com>
2025-03-30 11:13:56 +0530

    Refactor SiteHeader component: replace SidebarTrigger with IconStarFilled and adjust layout

--- client/retailradar/src/components/site-header.tsx
+++ client/retailradar/src/components/site-header.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import { IconStarFilled } from "@tabler/icons-react"
 
 export function SiteHeader({selectedItem}: { selectedItem: string }) {
   return (
     <header className="flex py-2 h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
+        <h6 className="mt-[7px]"><IconStarFilled/></h6>
         <Separator
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
