9d03b479c4fc5c0096338e1b64590ed01b31ce1e
Oshith Roshantha <oedirisuriya@gmail.com>
2025-04-07 12:22:29 +0530

    feat: Implement ComboBox component for product category selection and integrate into ProductSearch

--- client/retailradar/src/components/ProductSearch.tsx
+++ client/retailradar/src/components/ProductSearch.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { ComboBoxResponsive } from './ComboBox'
 
 export default function ProductSearch() {
   return (
     <div className="w-full h-full overflow-hidden">
       <div className="h-full overflow-y-auto scrollbar-hide py-3 px-5">
-        
+        <ComboBoxResponsive/>
       </div>
     </div>
   )
