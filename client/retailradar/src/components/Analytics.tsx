ad3e011ff9721307e782b84521d5d07c54e88851
Oshith Roshantha <oedirisuriya@gmail.com>
2025-04-11 21:28:51 +0530

    fix: Refactor Analytics component layout for improved styling and readability

--- client/retailradar/src/components/Analytics.tsx
+++ client/retailradar/src/components/Analytics.tsx
@@ -412,20 +412,20 @@ import { DeliveryMetrics } from "./Delivery"
     return (
       <div className="w-full h-full overflow-hidden">
         <div className="h-full overflow-y-auto scrollbar-hide py-3 px-5">
-          <div className="flex flex-row gap-4">
-            <Card className="w-[300px] px-4 bg-blue-800">
-              <CardTitle className="font-medium text-blue-200">Total Transactions</CardTitle>
-              <h1 className="mt-[-9%] text-white font-bold">{transactions.toLocaleString()}</h1>
-            </Card>
-            <Card className="w-[350px] px-4 bg-blue-800">
-              <CardTitle className="font-medium mt-1 text-blue-200">Start Date</CardTitle>
-              <h5 className="mt-[-7%] text-white font-bold">{formattedStartDate}</h5>
-            </Card>
-            <Card className="w-[350px] px-4 bg-blue-800">
-              <CardTitle className="font-medium mt-1 text-blue-200">End Date</CardTitle>
-              <h5 className="mt-[-7%] text-white font-bold">{formattedEndDate}</h5>
-            </Card>
-          </div>
+        <div className="flex gap-3">
+        <div className="bg-white p-4 rounded-lg shadow w-[350px]">
+          <h3 className="text-gray-500 text-sm font-medium">Total Transactions</h3>
+          <p className="text-6xl font-bold text-blue-800">{transactions.toLocaleString()}</p>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow w-[350px]">
+          <h3 className="text-gray-500 text-sm font-medium">Start Date</h3>
+          <p className="text-2xl font-bold text-[#6b67a0]">{formattedStartDate}</p>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow w-[350px]">
+          <h3 className="text-gray-500 text-sm font-medium">End Date</h3>
+          <p className="text-2xl font-bold text-[#6b67a0]">{formattedEndDate}</p>
+        </div>
+      </div>
           <Tabs className="w-[800px] pt-4" defaultValue="Customer Insights">
                 <TabsList className="grid w-full grid-cols-5 gap-1">
                     <TabsTrigger value="Customer Insights">Customer Insights</TabsTrigger>
