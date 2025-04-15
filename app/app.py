c1393c1179897328df59bf02b52caa4295e5a76c
Oshith Roshantha <oedirisuriya@gmail.com>
2025-04-15 18:53:45 +0530

    feat: Add CORS middleware for handling cross-origin requests

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routes.route import rrRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 retailradar = FastAPI()
 retailradar.include_router(rrRouter)
+
+retailradar.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     import uvicorn
