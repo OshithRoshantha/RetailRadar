c31590196c3273573bb32529fc58bd7a99f2c199
Oshith Roshantha <oedirisuriya@gmail.com>
2025-04-26 19:41:03 +0530

    fix: Add loading state and spinner to ProductSearch component while data is being fetched

--- client/retailradar/src/components/ProductSearch.tsx
+++ client/retailradar/src/components/ProductSearch.tsx
@@ -1,22 +1,25 @@
 import ProductGrid from "./ProductGrid";
 import React, { useEffect } from 'react';
+import loading from '../assets/img/loading.jpg';
 
 export default function ProductSearch() {
+    const [isDataLoaded, setIsDataLoaded] = React.useState(false);
+    const [productData, setProductData] = React.useState({});
 
-const [productData, setProductData] = React.useState({});
-useEffect(() => {
-    const fetchData = async () => {
-        const respose = await fetch('http://localhost:8000/retailradar/predict/sales', {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          })
-        const result = await respose.json();  
-        setProductData(result);
-    };
-    fetchData();
-  }, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            const respose = await fetch('http://localhost:8000/retailradar/predict/sales', {
+                method: 'GET',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            })
+            const result = await respose.json();  
+            setProductData(result);
+        };
+        fetchData();
+        setIsDataLoaded(true);
+    }, []);
 
   const productData = {
     "Result": [
@@ -253,6 +256,23 @@ useEffect(() => {
         }
     ]
 };
+
+    if (!isDataLoaded) {
+        return (
+        <div className="w-full h-full overflow-hidden">
+            <div className="h-full overflow-y-auto scrollbar-hide py-3 px-5">
+            <div className='flex flex-col items-center justify-center h-full'>
+                <img
+                src={loading}
+                alt="Image"
+                className="animate-spin w-16 h-16 mb-4"
+                />
+                <p className='text-gray-500 pt-5'>Hang tight! We’re getting things ready for you...</p>
+            </div>
+            </div>
+        </div>
+        );
+    }
 
   return (
     <div className="w-full h-full overflow-hidden">
