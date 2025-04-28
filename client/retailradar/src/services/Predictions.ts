17310888b46249e0a09963226c09caebe1bc3756
Oshith Roshantha <oedirisuriya@gmail.com>
2025-04-28 11:16:30 +0530

    feat: Enhance sales prediction function with axios request for data retrieval

--- client/retailradar/src/services/Predictions.ts
+++ client/retailradar/src/services/Predictions.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export const salesPredict = async () => {
-    
+    const response = await axios.get('http://localhost:8000/retailradar/predict/sales', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    return response.data;
 }
