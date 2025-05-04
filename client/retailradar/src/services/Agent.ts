c8017c4222d31044016eab1053382fcf73b713a7
Oshith Roshantha <oedirisuriya@gmail.com>
2025-05-04 12:49:32 +0530

    feat: Add agentReply function to handle agent queries via API

---
+++ client/retailradar/src/services/Agent.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const agentReply = async (ask) => {
+    const response = await axios.post('http://localhost:8000/retailradar/askAgent', {
+        question: ask
+    }, {
+        headers: {
+            'Content-Type': 'application/json',
+          },
+    });
+    return response.data;
+} 
