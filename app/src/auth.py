e588f51e3b82d971c1bf693dc7b99f21a931aacb
Oshith Roshantha <oedirisuriya@gmail.com>
2025-05-04 22:27:43 +0530

    fix: Update token expiration handling to use timezone-aware datetime

--- app/src/auth.py
+++ app/src/auth.py
@@ -1,5 +1,5 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 from dotenv import load_dotenv
 import os
@@ -11,9 +11,9 @@ ALGORITHM = os.getenv("ALGORITHM")
 def createToken(data: dict, expDelta: Optional[timedelta]=None):
     toEncode = data.copy()
     if expDelta:
-        exp = datetime.utcnow()+expDelta
+        exp = datetime.now(timezone.utc)+expDelta
     else:
-        exp = datetime.utcnow()+timedelta(minutes=15)
+        exp = datetime.now(timezone.utc)+timedelta(minutes=15)
     toEncode.update({"exp": int(exp.timestamp())})
     encodeJwt = jwt.encode(toEncode, KEY, algorithm=ALGORITHM)
     return encodeJwt
