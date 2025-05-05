6562bfdf3730d4ee7f7f44afc8e1e3a4fc788822
Oshith Roshantha <oedirisuriya@gmail.com>
2025-05-05 21:06:58 +0530

    feat: Add user registration function to database.py

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
+from userAccount import user
 
 load_dotenv()
 
@@ -9,3 +10,7 @@ dbName = os.getenv("DATABASE")
 
 client = AsyncIOMotorClient(uri)
 db = client[dbName]
+
+async def register(user: user):
+    newUser = user.dict()
+    await db["userProfiles"].insert_one(newUser)
